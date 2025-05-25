10bb25826220d7fc725f1274095cc8e05d2d5c06
Robert <robert2017martins@gmail.com>
2025-05-25 15:46:59 +0500

    fix DateRequest model

--- WebApi/Requests/DateRequest.cs
+++ WebApi/Requests/DateRequest.cs
@@ -3,6 +3,6 @@ namespace WebApi.Requests;
 public class DateRequest
 {
     public required int DayOfMonth { get; set; }
-    public DateTime? Date { get; set; }
+    public DateTime? Date { get; set; } = DateTime.Today;
     public bool? Adjust { get; set; }
 }
