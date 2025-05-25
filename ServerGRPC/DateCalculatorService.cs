f76b38c79317e035bd15ca5f140b6b4b9874f654
Robert <robert2017martins@gmail.com>
2025-05-25 15:46:31 +0500

    add DateCalculateService, proto file

---
+++ ServerGRPC/DateCalculatorService.cs
@@ -0,0 +1,39 @@
+using System.Globalization;
+using DateGrpc;
+using Grpc.Core;
+
+namespace ServerGRPC;
+
+public class DateCalculatorService : DateCalculator.DateCalculatorBase
+{
+    public override Task<DateResponse> GetNextReportDate(DateRequest request, ServerCallContext context)
+    {
+        DateTime newDate;
+        var baseDate = DateTime.ParseExact(request.Date, "yyyy-MM-dd", CultureInfo.InvariantCulture); 
+        try
+        {
+            newDate = new DateTime(baseDate.Year, baseDate.Month, request.DayOfMonth);
+            return Task.FromResult(new DateResponse { NextDate = newDate.ToString("yyyy-MM-dd") });
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            if (request.Adjust)
+            {
+                var theLastMonthDay = DateTime.DaysInMonth(baseDate.Year, baseDate.Month);
+                var resultDate = new DateTime(baseDate.Year, baseDate.Month, theLastMonthDay);
+                return Task.FromResult(new DateResponse { NextDate = resultDate.ToString("yyyy-MM-dd") });
+            }
+            else
+            {
+                var resultDate = new DateTime(baseDate.Year, baseDate.Month + 1, 1);
+                return Task.FromResult(new DateResponse { NextDate = resultDate.ToString("yyyy-MM-dd") });
+            }
+        }
+        catch (Exception e)
+        {
+            throw new RpcException(new Status(
+                StatusCode.InvalidArgument, 
+                $"Invalid date format. Expected YYYY-MM-DD. Error: {e.Message}"));
+        }
+    }
+}
